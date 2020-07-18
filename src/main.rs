01e4f6d31438593c7b5f5fd7532d9d90d3429f48
Alex Crawford <fb-unarchive@code.acrawford.com>
2020-07-18 16:03:07 -0700

    main: add initial photo processing
    This adds the creation timestamp and image description to the image and
    writes it to the output directory.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use anyhow::{Context, Result};
-use chrono::{DateTime, Utc};
+use anyhow::{anyhow, Context, Result};
+use chrono::naive::NaiveDateTime;
+use imagemeta::exif;
+use img_parts::{jpeg::Jpeg, ImageEXIF};
 use log::{debug, info, trace, LevelFilter};
 use std::fs::{self, File};
+use std::io::{BufReader, BufWriter, Cursor};
 use std::path::{Path, PathBuf};
 use structopt::StructOpt;
 
@@ -75,6 +78,8 @@ fn main() -> Result<()> {
     let albums = read_albums(&opts.input.join("album"))?;
     trace!("Albums: {:#?}", albums);
 
+    process_albums(albums, &opts.output)?;
+
     Ok(())
 }
 
@@ -94,4 +99,62 @@ fn read_albums(dir: &Path) -> Result<Vec<Album>> {
     }
 
     Ok(albums)
+}
+
+fn process_albums<A: IntoIterator<Item = Album>>(albums: A, out_dir: &Path) -> Result<()> {
+    debug!("Processing albums");
+
+    for album in albums {
+        let album_dir = out_dir.join(album.name);
+        fs::create_dir_all(&album_dir)?;
+
+        for photo in album.photos {
+            let description = photo.description.into_iter();
+            let comments = photo.comments.into_iter().filter_map(|c| {
+                c.comment.as_ref().map(|comment| {
+                    format!(
+                        r#""{}" -{} ({})"#,
+                        comment,
+                        c.author,
+                        c.timestamp.format("%F %r")
+                    )
+                })
+            });
+            let combined = description.chain(comments).collect::<Vec<_>>().join("\n");
+
+            let mut jpeg = Jpeg::read(&mut BufReader::new(File::open(&photo.path)?))
+                .map_err(|e| anyhow!("{}", e))?;
+
+            let exif = exif::Exif {
+                ifds: vec![exif::Ifd {
+                    id: 0,
+                    entries: vec![
+                        exif::Entry {
+                            tag: rexif::ExifTag::ImageDescription as u16,
+                            data: exif::EntryData::Ascii(combined),
+                        },
+                        exif::Entry {
+                            tag: rexif::ExifTag::DateTime as u16,
+                            data: exif::EntryData::Ascii(
+                                photo.timestamp.format("%Y:%m:%d %H:%M:%S").to_string(),
+                            ),
+                        },
+                    ],
+                    children: Vec::new(),
+                }],
+            };
+
+            trace!("Writing metadata for {}: {:#?}", photo.path.display(), exif);
+            let mut raw_exif = Cursor::new(Vec::new());
+            exif.encode(&mut raw_exif).map_err(|e| anyhow!("{}", e))?;
+            jpeg.set_exif(Some(raw_exif.into_inner()));
+
+            let out_path =
+                album_dir.join(photo.path.file_name().ok_or(anyhow!("missing filename"))?);
+            trace!("Outputting {}", out_path.display());
+            jpeg.write_to(&mut BufWriter::new(File::create(out_path)?))?;
+        }
+    }
+
+    Ok(())
 }
